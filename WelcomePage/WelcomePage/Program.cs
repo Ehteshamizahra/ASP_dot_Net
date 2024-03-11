f5b979b6010fa298d0047e8b4d392bc2ef925fa2
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-03-11 16:09:58 +0000

    New changes

---
+++ WelcomePage/WelcomePage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems is printed to the console.");
+        Console.WriteLine( "Remember, we’re \"accounting\" on you!");
+
+
+        Console.ReadLine();
+    }
+}
+
