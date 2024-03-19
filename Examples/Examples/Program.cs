322961fdd99586b0c8a141425105775cad8dc8be
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-03-19 17:18:38 +0000

    New assignment

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -8,7 +8,19 @@ class Program
     {
 
         bool number = 5 > 6;
-        Console.Write(number.ToString());
+        Console.WriteLine(number.ToString());
+        //Console.ReadLine();
+
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+        if (name != "Zahra")
+            Console.WriteLine("It's hard to look like her");
+        else
+            Console.WriteLine("You must be Zahra then");
+        
+        string results = 3 > 6 ? "are you crazy?" : "Makes sense";
+        Console.WriteLine(results);
         Console.ReadLine();
+
     }
 }
---
+++ Shipping quote for a package/Shipping quote for a package/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.WriteLine("What is the Package weight?");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+        else
+            Console.WriteLine("What is the Package width?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the Package height?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the Package length?");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int dimension_total = width + height + length;
+            int price = (width * height * length * weight) / 100;
+
+            if (dimension_total > 50)
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            else
+                Console.WriteLine("Your estimated total for shipping this package is: $" + price + "\nThank you!");
+
+        Console.ReadLine();
+
+
+    }
+}
+
+
+
