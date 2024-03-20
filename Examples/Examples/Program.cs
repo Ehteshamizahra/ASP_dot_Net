3a0ba0c2614bcd2da6cb5882d6e97cb64e73d696
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-03-20 10:48:43 +0000

    new assignment

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -7,19 +7,82 @@ class Program
     static void Main(string[] args)
     {
 
-        bool number = 5 > 6;
-        Console.WriteLine(number.ToString());
+        //bool number = 5 > 6;
+        //Console.WriteLine(number.ToString());
+        ////Console.ReadLine();
+
+        //Console.WriteLine("What's your name?");
+        //string name = Console.ReadLine();
+        //if (name != "Zahra")
+        //    Console.WriteLine("It's hard to look like her");
+        //else
+        //    Console.WriteLine("You must be Zahra then");
+
+        //string results = 3 > 6 ? "are you crazy?" : "Makes sense";
+        //Console.WriteLine(results);
         //Console.ReadLine();
 
-        Console.WriteLine("What's your name?");
-        string name = Console.ReadLine();
-        if (name != "Zahra")
-            Console.WriteLine("It's hard to look like her");
-        else
-            Console.WriteLine("You must be Zahra then");
+        string anotherGO;
+        Console.WriteLine("Which month of year sprint starts?");
+        String month = Console.ReadLine().ToLower();
+        bool answer = month ==  "march" ;
         
-        string results = 3 > 6 ? "are you crazy?" : "Makes sense";
-        Console.WriteLine(results);
+        do
+        {
+            switch (month)
+            {
+                case "march":
+                    Console.WriteLine("That's correct!");
+                    answer = false;
+                    break;
+                case "april":
+                case "february":
+                    Console.WriteLine("You are very close!");
+                    Console.WriteLine("Do you want to have another go? answer with yes or no");
+                    anotherGO = Console.ReadLine().ToLower();
+                    if (anotherGO == "yes")
+                        answer = true;
+                    else
+                        answer = false;
+                    Console.WriteLine("Which month of year sprint starts?");
+                    month = Console.ReadLine().ToLower();
+                    break;
+                case "june":
+                case "july":
+                case "auhust":
+                case "september":
+                case "october":
+                case "november":
+                case "december":
+                case "january":
+                case "may":
+                    Console.WriteLine("Not quite!");
+                    Console.WriteLine("Do you want to have another go? answer with yes or no");
+                    anotherGO = Console.ReadLine().ToLower();
+                    if (anotherGO == "yes")
+                        answer = true;
+                    else
+                        answer = false;
+                    Console.WriteLine("Which month of year sprint starts?");
+                    month = Console.ReadLine().ToLower();
+                    break;
+                default:
+                    Console.WriteLine("The entry is not a month");
+                    Console.WriteLine("Do you want to have another go? answer with yes or no");
+                    anotherGO = Console.ReadLine().ToLower();
+                    if (anotherGO == "yes")
+                        answer = true;
+                    else
+                        answer = false;
+                    Console.WriteLine("Which month of year sprint starts?");
+                    month = Console.ReadLine().ToLower();
+                    break;
+            }
+
+        }
+        while (answer);
+            
+
         Console.ReadLine();
 
     }
