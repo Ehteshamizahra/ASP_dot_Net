7395932b373ac107a627957b10f6dc3c7e730217
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-04-08 10:09:57 +0100

    New file

---
+++ Assignment_Overlaoding_method/Assignment_Overlaoding_method/Program.cs
@@ -0,0 +1,44 @@
+﻿//Assignment on overloading methods
+
+using System;
+
+
+namespace Assignment_Overlaoding_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Define an integer variable and instantiate the integer maths operation
+            int deg = 10;
+            Console.WriteLine(mathOp(deg));
+            //Define a decimal variable and instantiate the decimal maths operation
+            decimal rad =10.10m;
+            Console.WriteLine(mathOp(rad));
+            //Define a string variable and instantiate the string maths operation
+            string stringAngle = "20";
+            Console.WriteLine(mathOp(stringAngle));
+            Console.ReadLine();
+        }
+        //Create the first maths operation method that takes an integer as an input and return an integer
+        public static int mathOp(int deg)
+        {
+            int result = deg * (((int)Math.PI) / 180);
+            return result;
+        }
+        //Create the first maths operation method that takes a decimal as an input and return an integer
+        public static int mathOp(Decimal rad)
+        {
+            int result = Convert.ToInt32(rad * 180 / (decimal)Math.PI);
+            return result;
+        }
+        //Create the first maths operation method that takes a string as an input and return an integer
+        public static int mathOp(string rad)
+        {
+            int result = Convert.ToInt32(rad) * 180 / (int)Math.PI;
+            return result;
+        }    
+
+    }
+}
--- blackJack_game/blackJack_game/Program.cs
+++ blackJack_game/blackJack_game/Program.cs
@@ -11,7 +11,10 @@ namespace blackJack_game
         static void Main(string[] args)
         {
             Deck deckOne = new Deck();
-            deckOne = Shuffle(deckOne); // call method shuffle
+            int timesShuffled = 0;
+
+            deckOne = Shuffle(deckOne, out timesShuffled,2) ; // call method shuffle
+            //deckOne = Shuffle(deck: deckOne, times: 3); // Named parameter
 
             foreach (Card card in deckOne.Cards)
             {
@@ -19,26 +22,52 @@ namespace blackJack_game
 
             }
             Console.WriteLine(deckOne.Cards.Count);
+            Console.WriteLine("Times shuffled: " + timesShuffled);
             Console.ReadLine();
         }
 
 
         //Creating a method to allow shuffle the deck of cards
-        public static Deck Shuffle(Deck deck)   //public: so it's accassable, static: We don't have to create an object program before calling this,Deck: That's the type of data it's returing, Shuffle: the name of the function, Deck deck: takes a prameter of types Deck with variable named deck
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)   //public: so it's accassable, static: We don't have to create an object program before calling this,
+                                                                                     //Deck: That's the type of data it's returing, Shuffle: the name of the function,
+                                                                                     //Deck deck: takes a prameter of types Deck with variable named deck
+                                                                                     //out int timeshuffled: 
+                                                                                     //int times =1: an optional argument
         {
-            Random random = new Random();
-            List<Card> tempList = new List<Card>();
 
-            while (deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomInd = random.Next(0, deck.Cards.Count);
-                tempList.Add(deck.Cards[randomInd]);
-                deck.Cards.RemoveAt(randomInd);
+                Random random = new Random();
+                List<Card> tempList = new List<Card>();
+                timesShuffled++;
+                while (deck.Cards.Count > 0)
+                {
+                    
+                    int randomInd = random.Next(0, deck.Cards.Count);
+                    tempList.Add(deck.Cards[randomInd]);
+                    deck.Cards.RemoveAt(randomInd);
+                }
+                deck.Cards = tempList;
             }
-            deck.Cards = tempList;
             return deck;
-        
+
+
         }
+        //public static Deck Shuffle(Deck deck, int times) //Overloading method. Run shuffle multiple times. As long as the arguments are different  the program knows which one to call
+        //{
+
+        //    for (int i = 0; i< times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+
+        //    return deck;
         
+        
+        //}
+
+
+
     }
 }
