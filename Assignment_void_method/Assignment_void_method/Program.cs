3cd4181c81aeda7458819161a623bfd0a9db1121
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-04-08 14:26:06 +0100

    New changes

--- Assignment_void_method/Assignment_void_method/Program.cs
+++ Assignment_void_method/Assignment_void_method/Program.cs
@@ -10,17 +10,22 @@ namespace Assignment_void_method
     {
         static void Main(string[] args)
         {
+
+            //instantiate class birds
             birds bird = new birds();
+
+            //Define two variables and pass them as arguments to the mathsOp class
             int input1 = 1;
             int input2 = 2;
             birds.mathsOp(input1:input1, input2:input2);
 
             Console.ReadLine();
         }
-
+        //Create a class called birds
         public class birds
 
         {
+            //CReate a void method with two inputs
             public static void mathsOp(int input1, int input2)
             {
                 int result = (int)Math.Log10(input1);
