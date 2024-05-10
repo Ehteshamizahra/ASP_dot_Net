e25b0df27ce9d5b2f86500b34d068d7b93557381
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-05-11 00:42:16 +0100

    New file

---
+++ Assignment_Cons_ChainConstructors/Assignment_Cons_ChainConstructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Cons_ChainConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string riverName = "Thame";//Creating a constant
+
+            var riverLength = new Dictionary<string, int>();//Creating a variable
+
+        }
+
+        //example of chained constructors
+        public class animals
+        {
+            public string animalName;
+            public int animalLength;
+
+            //Constructor 1
+            public animals(string animalName) : this(animalName, 2)
+            {
+
+            }
+            public animals(string animalName, int animalLength)
+            {
+                this.animalName = "animalName";
+                this.animalLength = 5;
+            }
+        }
+    }
+}
