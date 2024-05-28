4ffe2e30a7dfcf171167df3047442f85795d15a6
Ehteshamizahra <62758797+Ehteshamizahra@users.noreply.github.com>
2024-05-28 11:50:25 +0100

    new file

--- NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,28 +10,85 @@ namespace NewsLetterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NewsLetter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         public ActionResult Index()
         {
             return View();
         }
         [HttpPost] //anytime you post method include ths tag to notify the coder about the impact of 'posting'
-        public ActionResult SignUp(string FirstName, string LastName, string EmailAddress)
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
         {
-            return null;
+            if (string.IsNullOrEmpty(firstName)|| string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) )
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+                return View("Success");
+            }
         }
 
-        public ActionResult About()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your application description page.";
+            // ADO.NET which is commented out and replaced by entity framework calls
+            string querystring = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
+            List<NewsLetterSignUp> signups = new List<NewsLetterSignUp>();
 
-            return View();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand(querystring, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsLetterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAdddress"].ToString();
+                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+                    signups.Add(signup);
+                }
+
+            }
+            var signupVms = new List<SignupVm>(); // create a new list of view models
+            //Limiting the information passed to the view
+            //This is using the ViewModels folder to create the model that will be viewed, so not all properties in the Model has to be passed to the view
+            foreach (var signup in signups) // map the view models from the model to view model
+            {
+                var signupVm = new SignupVm();
+                signup.FirstName = signup.FirstName;
+                signupVm.LastName = signup.LastName;
+                signupVm.EmailAddress = signupVm.EmailAddress;
+                signupVms.Add(signupVm);
+            }
+
+
+            return View(signupVms); //pass viewmodel to the view
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+    }
     }
 }
